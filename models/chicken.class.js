796022a15ba45ff868c7cbb3a6f3697e3d2f80a5
RobertAliaj <robertaliaj97@gmail.com>
2023-02-15 23:04:40 +0100

    save

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -8,6 +8,7 @@ class Chicken extends MovableObject {
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
     ];
+    world;
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
@@ -19,13 +20,15 @@ class Chicken extends MovableObject {
         this.animate();
     }
 
+    
     animate() {
         setInterval(() => {
             this.moveLeft();
         }, 1000 / 200); // 200 mal pro sekunde 
-
+        
         setInterval(() => {
             this.playAnimations(this.IMAGES_WALKING);
         }, 100);
     }
+    
 }
